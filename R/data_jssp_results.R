d18c3306b3e6311cbbc42235e53fe1d8ce7464c8
Thomas Weise <tweise@hfuu.edu.cn>
2019-08-20 05:48:29 +0800

    first update including also parallelism information

--- R/data_jssp_results.R
+++ R/data_jssp_results.R
@@ -14,20 +14,32 @@
 #' \item{inst.id}{the ID of the problem instance solved}
 #' \item{inst.opt.bound.lower}{lower bound of the optimal objective value}
 #' \item{system.cpu.name}{the name of the CPU of the system on which the experiment was run}
-#' \item{system.cpu.cores}{the number of cores of the CPU}
-#' \item{system.cpu.threads}{the number of threads of the CPU}
+#' \item{system.cpu.n}{the number of CPUs used}
+#' \item{system.cpu.cores}{the number of cores of per CPU}
+#' \item{system.cpu.threads}{the number of threads of per CPU}
 #' \item{system.cpu.mhz}{the clock speed of the CPU in MHz}
+#' \item{system.runs.are.parallel}{are the single runs making use of parallelization, i.e., do they use multiple threads?}
 #' \item{system.memory.mb}{the memory size of the system in MB}
+#' \item{system.os.name}{the name of the CPU of the system on which the experiment was run}
+#' \item{system.compiler.name}{the name of the compiler used to compile the programs for the experiment}
 #' \item{system.programming.language.name}{the name of the programming language in which the program was written}
+#' \item{system.external.tools.list}{the name of the external tools, such as deterministic solvers or CPLEX, whatever was used in the experiments}
 #' \item{n.runs}{the total number of repetitions / runs performed}
 #' \item{best.f.min}{the minimum value of the best objective value ever reached during the run}
 #' \item{best.f.mean}{the arithmetic mean of the best objective value ever reached during the run}
+#' \item{best.f.med}{the median of the best objective value ever reached during the run}
+#' \item{best.f.mode}{the mode, i.e., the most frequent value, of the best objective value ever reached during the run}
+#' \item{best.f.max}{the maximum value of the best objective value ever reached during the run}
+#' \item{best.f.sd}{the standard deviation of the best objective value ever reached during the run}
 #' \item{total.time.mean}{the arithmetic mean of the total consumed runtime of the run, i.e., the time until the run was terminated}
+#' \item{reach.best.f.min.time.min}{the minimum value of the time needed by the runs which discovered the best solution among all runs (best.f.min) until they reached said solution}
 #'}
 #'
 #' @keywords Job Shop Scheduling, JSSP, results
 #'
-#' @references  Pongchairerks P (2019). “A Two-Level Metaheuristic Algorithm for the Job-Shop Scheduling Problem.” Complexity, 2019(8683472), 1-11. doi: 10.1155/2019/8683472 (URL: http://doi.org/10.1155/2019/8683472), <URL: http://www.hindawi.com/journals/complexity/2019/8683472/>.
+#' @references  Cruz-Chávez MA, Cruz Rosales MH, Zavala-Díaz JC, Aguilar JAH, Rodrıguez-Leó A, Avelino JCP, Orziz MEL, Salinas OH (2019). “Hybrid Micro Genetic Multi-Population Algorithm With Collective Communication for the Job Shop Scheduling Problem.” IEEE Access, 7, 82358-82376. doi: 10.1109/ACCESS.2019.2924218 (URL: http://doi.org/10.1109/ACCESS.2019.2924218), <URL: http://ieeexplore.ieee.org/document/8743353>.
+#' 
+#' Pongchairerks P (2019). “A Two-Level Metaheuristic Algorithm for the Job-Shop Scheduling Problem.” Complexity, 2019(8683472), 1-11. doi: 10.1155/2019/8683472 (URL: http://doi.org/10.1155/2019/8683472), <URL: http://www.hindawi.com/journals/complexity/2019/8683472/>.
 #' 
 #'
 #' @examples
--- README.md
+++ README.md
@@ -4,4 +4,11 @@ A repository with a data set including instances and results from literature.
 While the raw data is provided as text files, it is also compiled in an `R` package with an API around it.
 The goal is to have an up-date-able archive of the state-of-the-art results, directly linked with BibTeX entries and the functionality to generate result tables and to compare algorithms with said state-of-the-art.
 
+While all data is easily accessible within the `R` package, you can also access the raw data as follows:
+
+- the [results from the literature](https://raw.githubusercontent.com/thomasWeise/jsspInstancesAndResults/master/data-raw/results/all_results.txt) as CSV file
+- the [instance information file](https://raw.githubusercontent.com/thomasWeise/jsspInstancesAndResults/master/data-raw/instances/instances.txt) as CSV file, mainly with infos from Jelke Jeroen van&nbsp;Hoorn's website <http://jobshop.jjvh.nl>
+- the [instances from ORLib](https://raw.githubusercontent.com/thomasWeise/jsspInstancesAndResults/master/data-raw/instances/instance_data_orlib.txt)
+- the [raw BibTeX file with all references to literature](https://raw.githubusercontent.com/thomasWeise/jsspInstancesAndResults/master/data-raw/bibliography/bibliography.bib)
+
 Many of the information about the problem instances are taken from incredibly great website <http://jobshop.jjvh.nl> run by Jelke Jeroen van&nbsp;Hoorn, while the results are taken from their individual publications.
