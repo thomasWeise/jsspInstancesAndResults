bc3591a723c022c837bf41ab55652eec789027c6
Thomas Weise <tweise@hfuu.edu.cn>
2019-08-21 10:24:18 +0800

    some more results

--- R/data_jssp_results.R
+++ R/data_jssp_results.R
@@ -57,6 +57,8 @@
 #' 
 #' Akram K, Kamal K, Zeb A (2016). “Fast Simulated Annealing Hybridized with Quenching for Solving Job Shop Scheduling Problem.” Applied Soft Computing Journal (ASOC), 49, 510-523. doi: 10.1016/j.asoc.2016.08.037 (URL: http://doi.org/10.1016/j.asoc.2016.08.037).
 #' 
+#' Angel JM, Martínez MR, Castillo LRM, Solis LS (2014). “Un Modelo Híbrido de Inteligencia Computacional para Resolver el Problema de Job Shop Scheduling.” Research in Computing Science, 79(Advances in Intelligent Information Technologies), 9-20. <URL: http://www.rcs.cic.ipn.mx/2014_79/RCS_79_2014.pdf>.
+#' 
 #' Amirghasemi M, Zamani R (2015). “An Effective Asexual Genetic Algorithm for Solving the Job Shop Scheduling Problem.” Computers & Industrial Engineering, 83, 123-138. doi: 10.1016/j.cie.2015.02.011 (URL: http://doi.org/10.1016/j.cie.2015.02.011).
 #' 
 #' Bierwirth C (1995). “A Generalized Permutation Approach to Job Shop Scheduling with Genetic Algorithms.” Operations-Research-Spektrum (OR Spectrum), 17(2-3), 87-92. doi: 10.1007/BF01719250 (URL: http://doi.org/10.1007/BF01719250), <URL: http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.52.7392&type=pdf>.
@@ -81,15 +83,21 @@
 #' 
 #' Kurdi M (2015). “A New Hybrid Island Model Genetic Algorithm for Job Shop Scheduling Problem.” Computers & Industrial Engineering, 88, 273-283. doi: 10.1016/j.cie.2015.07.015 (URL: http://doi.org/10.1016/j.cie.2015.07.015).
 #' 
+#' Kulkarni K, Venkateswaran J (2014). “Iterative Simulation and Optimization Approach for Job Shop Scheduling.” In Buckley SJ, Miller JA (eds.), Proceedings of the 2014 Winter Simulation Conference, December 7-10, 2014, Savannah, GA, USA, 1620-1631. doi: 10.1109/WSC.2014.7020013 (URL: http://doi.org/10.1109/WSC.2014.7020013), <URL: https://www.anylogic.com/upload/iblock/5aa/5aa2987b839049668eeef8a21c811e6b.pdf>.
+#' 
 #' Li L, Weng W, Fujimura S (2017). “An Improved Teaching-Learning-based Optimization Algorithm to Solve Job Shop Scheduling Problems.” In Zhu G, Yao S, Cui X, Xu S (eds.), 16th IEEE/ACIS International Conference on Computer and Information Science (ICIS'17), May 24-26, 2017, Wuhan, China, 797-801. ISBN 978-1-5090-5507-4, doi: 10.1109/ICIS.2017.7960101 (URL: http://doi.org/10.1109/ICIS.2017.7960101).
 #' 
 #' Miller-Todd J, Steinhöfel K, Veenstra P (2018). “Firefly-Inspired Algorithm for Job Shop Scheduling.” In Böckenhauer H, Komm D, Unger W (eds.), Adventures Between Lower Bounds and Higher Altitudes - Essays Dedicated to Juraj Hromkovič on the Occasion of His 60th Birthday, volume 11011 series Lecture Notes in Computer Science (LNCS), 423-433. Springer. ISBN 978-3-319-98354-7, doi: 10.1007/978-3-319-98355-4_24 (URL: http://doi.org/10.1007/978-3-319-98355-4_24).
+#' 
+#' Nguyen S, Zhang M, Johnston M, Tan KC (2013). “A Computational Study of Representations in Genetic Programming to Evolve Dispatching Rules for the Job Shop Scheduling Problem.” IEEE Transactions on Evolutionary Computation (TEVC), 17(5), 621-639. doi: 10.1109/TEVC.2012.2227326 (URL: http://doi.org/10.1109/TEVC.2012.2227326).
 #' 
 #' Pongchairerks P (2014). “Variable Neighbourhood Search Algorithms Applied to Job-Shop Scheduling Problems.” International Journal of Mathematics in Operational Research (IJMOR), 6(6), 752-774. doi: 10.1504/IJMOR.2014.065421 (URL: http://doi.org/10.1504/IJMOR.2014.065421).
 #' 
 #' Pongchairerks P (2019). “A Two-Level Metaheuristic Algorithm for the Job-Shop Scheduling Problem.” Complexity, 2019(8683472), 1-11. doi: 10.1155/2019/8683472 (URL: http://doi.org/10.1155/2019/8683472), <URL: http://www.hindawi.com/journals/complexity/2019/8683472/>.
 #' 
 #' Peng B, Lü Z, Cheng TCE (2015). “A Tabu Search/Path Relinking Algorithm to Solve the Job Shop Scheduling Problem.” Computers & Operations Research, 53, 154-164. doi: 10.1016/j.cor.2014.08.006 (URL: http://doi.org/10.1016/j.cor.2014.08.006), A February 2014 preprint is available as arXiv:1402.5613v1 [cs.DS], <URL: http://arxiv.org/abs/1402.5613>.
+#' 
+#' Qiu X, Lau HYK (2014). “An AIS-based Hybrid Algorithm for Static Job Shop Scheduling Problem.” Journal of Intelligent Manufacturing, 25(3), 489-503. doi: 10.1007/s10845-012-0701-2 (URL: http://doi.org/10.1007/s10845-012-0701-2).
 #' 
 #' Sharma N, Sharma H, Sharma A (2018). “Beer froth Artificial Bee Colony Algorithm for Job-Shop Scheduling Problem.” Applied Soft Computing Journal (ASOC), 68, 507-524. doi: 10.1016/j.asoc.2018.04.001 (URL: http://doi.org/10.1016/j.asoc.2018.04.001).
 #' 
